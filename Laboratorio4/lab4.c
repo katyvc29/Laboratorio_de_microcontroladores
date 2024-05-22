52f7963b12d7379e807640ff4ddc2d499935e9ea
SuJH20 <susanmile20@gmail.com>
2024-05-22 16:23:00 -0600

    Se incluyen librerias que se espera sean utilizadas

---
+++ Laboratorio4/lab4.c
@@ -0,0 +1,17 @@
+//Librerias a utilizar 
+#include "usart.h"
+#include "spi.h"
+#include "gpio.h"
+#include "dac.h"
+#include "adc.h"
+#include "rcc.h"
+#include "gfx.h"
+#include "lcd-spi.h"
+#include "sdram.h"
+#include "console.h"
+#include "clock.h"
+
+//Librerias del micro
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/spi.h>
