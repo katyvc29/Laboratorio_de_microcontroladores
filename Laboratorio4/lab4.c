51064b06340343437ac86f06df98592c4aba1cb1
SuJh20 <susanmile20@gmail.com>
2024-05-24 22:37:36 -0600

    Se agregan definiciones de variables para los registros de datos del giroscopio

--- Laboratorio4/lab4.c
+++ Laboratorio4/lab4.c
@@ -33,14 +33,22 @@
 #define GYR_STATUS_REG 0x27 //Registra el estado
 
 //Definiciones para configurar el giroscopio
-#define GYR_CTRL_REG1 0X20 //Configura el registro de control 1
-#define GYR_CTRL_REG1_PD (1 << 3) //Modo de encendido
-#define GYR_CTRL_REG1_XEN (1 << 1) //Habilita el eje X
-#define GYR_CTRL_REG1_YEN (1 << 0) //Habilita el eje Y
-#define GYR_CTRL_REG1_ZEN (1 << 2) //Habilita el eje Z
+#define GYR_CTRL_REG1           0X20 //Configura el registro de control 1
+#define GYR_CTRL_REG1_PD        (1 << 3) //Modo de encendido
+#define GYR_CTRL_REG1_XEN       (1 << 1) //Habilita el eje X
+#define GYR_CTRL_REG1_YEN       (1 << 0) //Habilita el eje Y
+#define GYR_CTRL_REG1_ZEN       (1 << 2) //Habilita el eje Z
 #define GYR_CTRL_REG1_BW_SHIFTH 4 //Cambios en el ancho de banda
-#define GYR_CTRL_REG4 0x23 //Configura el registro de control 4
+#define GYR_CTRL_REG4           0x23 //Configura el registro de control 4
 #define GYR_CTRL_REG4_FS_SHIFTH 4 //Cambios en la escala 
+
+//Direcciones de los registros de datos del girocospio
+#define GYR_OUT_X_L 0X28
+#define GYR_OUT_X_H 0X29
+#define GYR_OUT_Y_L 0X2A
+#define GYR_OUT_Y_H 0X2B
+#define GYR_OUT_Z_L 0X2C
+#define GYR_OUT_Z_H 0X2D
 
 
 //Funcion principal del programa 
